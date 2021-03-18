29bd00db870719f357e24a95170e37a2b1781559
dalacorte <victor.dalacorte@etec.sp.gov.br>
2021-03-18 20:17:18 -0300

    typescript

---
+++ app/ts/controllers/negociacao-controller.ts
@@ -0,0 +1,27 @@
+class NegociacaoController{
+    
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+
+    constructor(){
+
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#data');
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
+        
+    }
+}
