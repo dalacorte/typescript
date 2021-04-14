06fb019c24e2e4e8f9286cbacea06943ae330985
Victor Dalacôrte <victor.dalacorte@etec.sp.gov.br>
2021-04-14 17:34:37 -0300

    typescript

--- app/ts/app.ts
+++
@@ -1,5 +0,0 @@
-import { NegociacaoController } from './controllers/negociacao-controller';
-
-const controller = new NegociacaoController();
-
-$('.form').submit(controller.adiciona.bind(controller));
